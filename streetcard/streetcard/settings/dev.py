e8bfd149bf34e96b075dcfb92f4ed9f7bfea6e7b
streetcard <streetcard.org@gmail.com>
2023-03-31 16:04:07 +0000

    initial upload

---
+++ streetcard/streetcard/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-yjwnckt1x*=2_9q4cdkgc!fx9xb!*o79)14a)^4asl4kgh#0qq'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
