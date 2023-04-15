fae254c608b1e54ad3aa52d9823e2d27306346bc
calfaro7 <105833802+calfaro7@users.noreply.github.com>
2023-04-15 07:20:21 -0700

    updated email to use sendgird - need to test

--- streetcard/home/forms.py
+++ streetcard/home/forms.py
@@ -2,6 +2,12 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    firstname = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name',
+                                                                             'style': 'width:200px; inline:block'}))
+    lastname = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name',
+                                                            'style': 'width:200px'}))
+    from_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email Address',
+                                                                             'style': 'width:200px'}))
+    subject = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Subject',
+                                                                             'style': 'width:200px'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Email Body'}), required=True)
